80221006046ed96bfb0b382d4e7cd00beba03305
Ariel Fuggini <afuggini@users.noreply.github.com>
2016-08-05 12:09:28 -0300

    Updated docker image

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -15,7 +15,7 @@ docker rm -f $APPNAME-frontend
 
 # We don't need to fail the deployment because of a docker hub downtime
 set +e
-docker pull afuggini/meteord:base
+docker pull exkazuu/meteord:base
 set -e
 
 if [ "$USE_LOCAL_MONGO" == "1" ]; then
@@ -29,7 +29,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
     --name=$APPNAME \
-    afuggini/meteord:base
+    exkazuu/meteord:base
 else
   docker run \
     -d \
@@ -39,7 +39,7 @@ else
     --hostname="$HOSTNAME-$APPNAME" \
     --env-file=$ENV_FILE \
     --name=$APPNAME \
-    afuggini/meteord:base
+    exkazuu/meteord:base
 fi
 
 <% if(typeof sslConfig === "object")  { %>
